d20d87e0812356b89570e2355265520ed56fa141
Somula Jaswanth Reddy <88081034+jaswanthreddy-007@users.noreply.github.com>
2023-08-06 04:49:56 +0530

    Update Election.sol

--- project/page4/Election.sol
+++ project/page4/Election.sol
@@ -1,7 +1,6 @@
 pragma solidity 0.5.16;
 
 contract Election {
-    // Model a Candidate
     struct Candidate {
         uint id;
         string name;
@@ -9,23 +8,17 @@ contract Election {
     }
     uint256 lastRun=now;
 
-
-    // Store accounts that have voted
     mapping(address => bool) public voters;
-    // Store Candidates
-    // Fetch Candidate
     mapping(uint => Candidate) public candidates;
-    // Store Candidates Count
     uint public candidatesCount;
 
-    // voted event
     event votedEvent (
         uint indexed _candidateId
     );
 
     constructor  () public {
-        addCandidate("Candidate 1");
-        addCandidate("Candidate 2");
+        addCandidate("Narendra Modi");
+        addCandidate("Donald Trump");
     }
 
     function addCandidate (string memory _name) private {
@@ -35,20 +28,14 @@ contract Election {
 
     function vote (uint _candidateId) public {
 
-        require(block.timestamp - lastRun < 2 minutes, 'Time up');
-        // require that they haven't voted before
+        require(block.timestamp - lastRun < 10000 minutes, 'Time up');
         require(!voters[msg.sender]);
 
-        // require a valid candidate
         require(_candidateId > 0 && _candidateId <= candidatesCount);
 
-        // record that voter has voted
         voters[msg.sender] = true;  
-
-        // update candidate vote Count
         candidates[_candidateId].voteCount ++;
 
-        // trigger voted event
         emit votedEvent(_candidateId);
     }
 }
